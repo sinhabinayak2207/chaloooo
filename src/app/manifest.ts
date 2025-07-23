95422d79387c6f562041983059c6174f6979b84f
sinhabinayak2207 <71455686+sinhabinayak2207@users.noreply.github.com>
2025-07-24 00:31:53 +0530

    Update manifest.ts

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,4 @@
-import { MetadataRoute } from 'next'
- 
-export default function manifest(): MetadataRoute.Manifest {
+export default function manifest() {
   return {
     name: 'OCC World Trade',
     short_name: 'OCC World Trade',
